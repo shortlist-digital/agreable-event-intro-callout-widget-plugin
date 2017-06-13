c8cdc8cdbf7d06cf53c56e0d5ef0ecb63f6f7bbf
Leo <leonoragilmour@sky.com>
2017-06-13 11:57:33 +0100

    init

---
+++ event-intro-callout-widget-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: Event Intro Callout Widget Plugin
+* Plugin URI: http://github.com/shortlist-digital/event-intro-callout-widget-plugin
+* Description: A plugin to add an event intro callout widget
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class EventIntroCalloutWidgetPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new EventIntroCalloutWidgetPlugin();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Event Intro Callout Widget Plugin
+A plugin to add an event intro callout widget
